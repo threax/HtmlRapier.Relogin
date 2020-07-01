abb3d20f7f732a2d9d3dc74c4f2463333980e37a
Andrew Piper <andrew@threax.com>
2020-07-01 14:00:40 -0400

    Changed LoggedInPage to be centralized logic called from an external page runner.

--- src/LoggedInPage.ts
+++ src/LoggedInPage.ts
@@ -1,10 +1,10 @@
-﻿///<amd-module name="hr.relogin.logged-in-page"/>
-
-import * as loginPopup from 'hr.relogin.LoginPopup';
+﻿import * as loginPopup from 'hr.relogin.LoginPopup';
 
 var data: loginPopup.ILoginMessage = {
         type: loginPopup.MessageType,
         success: true
     };
 
-parent.postMessage(JSON.stringify(data), "*");
+export function alertLoggedIn() {
+    parent.postMessage(JSON.stringify(data), "*");
+}
