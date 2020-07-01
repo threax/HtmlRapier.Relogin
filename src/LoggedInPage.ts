ab0ba7997ec3acb3aeb97b68f5adeaafce2dc5a7
Andrew Piper <andrew@threax.com>
2020-07-01 11:45:04 -0400

    Changed how logged in page works. No longer backward compatible.

--- src/LoggedInPage.ts
+++ src/LoggedInPage.ts
@@ -1,11 +1,10 @@
-﻿///<amd-module name="hr.relogin.logged-in-page"/>
-//This amd name can stay since this file is itself an entry point. This can keep b/c with older sites.
-
-import * as loginPopup from './LoginPopup';
+﻿import * as loginPopup from './LoginPopup';
 
 var data: loginPopup.ILoginMessage = {
         type: loginPopup.MessageType,
         success: true
     };
 
-parent.postMessage(JSON.stringify(data), "*");
+export function alertLoggedIn() {
+    parent.postMessage(JSON.stringify(data), "*");
+}
