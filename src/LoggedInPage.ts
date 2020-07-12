f5fbc0ffe474aec9da1b964aff27b6f8b83d02ca
Andrew Piper <andrew@threax.com>
2020-07-12 16:56:22 -0400

    Send message to the top window in addition to the parent.

--- src/LoggedInPage.ts
+++ src/LoggedInPage.ts
@@ -1,10 +1,13 @@
 ﻿import * as loginPopup from 'hr.relogin.LoginPopup';
 
 var data: loginPopup.ILoginMessage = {
-        type: loginPopup.MessageType,
-        success: true
-    };
+    type: loginPopup.MessageType,
+    success: true
+};
 
 export function alertLoggedIn() {
     parent.postMessage(JSON.stringify(data), "*");
+    if (parent !== top) { //If iframe in iframe, a loop through parent here will just infinite loop for some reason.
+        top.postMessage(JSON.stringify(data), "*");
+    }
 }
