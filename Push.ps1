8ceb10ea3d130d35cf327f14917b33bbc2c09556
Andrew Piper <andrew@threax.com>
2021-07-21 15:10:58 -0400

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/HtmlRapier.Relogin.git
+git push https://github.com/threax/HtmlRapier.Relogin.git --tags
+git push https://gitlab.com/threax-software/HtmlRapier.Relogin.git
+git push https://gitlab.com/threax-software/HtmlRapier.Relogin.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.Relogin
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.Relogin --tags
